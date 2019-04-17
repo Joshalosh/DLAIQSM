31902a881817e5154a2b39276a1abd44450e5707
Joshalosh <josh.jessopsmith@gmail.com>
2019-04-18 00:04:36 +1000

    Add objective truth to main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main()
+{
+    int x(1);
+    int y(1);
+    std::cout << "An objective truth is that " << x
+    << " + " << y << " = " << x + y << std::endl;
+    return 0;
+}
