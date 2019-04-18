a083711a62e0cacab1249efd42c67335e2954ec3
Joshalosh <josh.jessopsmith@gmail.com>
2019-04-18 15:46:05 +1000

    Update DLAIQSM.cpp

--- src/DLAIQSM.cpp
+++ src/DLAIQSM.cpp
@@ -1,10 +1,12 @@
+#include <iostream>
+
 #define Modulator }
 #define Intelligent {
 #define Deep int
 #define Learning main
 #define Artificial (void)
 #define Statistics return 0;
-#define Quantum printf("Let's all modulate statistics through the power of Deep-Learning, artificial intelligence and quantum computation. Huzzah!\n");
+#define Quantum std::cout <<"Let's all modulate statistics through the power of Deep-Learning, artificial intelligence and quantum computation. Huzzah!" << std::endl;
 
 Deep Learning Artificial
 Intelligent
