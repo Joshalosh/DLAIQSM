9b2bf57aa256bb320780af1147ec4d6d24ef3a9b
Sonofotis <39609800+Sonofotis@users.noreply.github.com>
2019-04-18 15:31:52 +1000

    Initial Deep-Learning Artificial-Intelligent Quantum Statistics Modulator entry

---
+++ src/DLAIQSM.cpp
@@ -0,0 +1,13 @@
+#define Modulator }
+#define Intelligent {
+#define Deep int
+#define Learning main
+#define Artificial (void)
+#define Statistics return 0;
+#define Quantum printf("Let's all modulate statistics through the power of Deep-Learning, artificial intelligence and quantum computation. Huzzah!\n");
+
+Deep Learning Artificial
+Intelligent
+  Quantum
+  Statistics 
+Modulator
