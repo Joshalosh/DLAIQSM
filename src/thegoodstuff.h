d1f23bf35785efb3bc65ae2805342ccc0e6ee695
Joshalosh <josh.jessopsmith@gmail.com>
2019-04-28 18:34:36 +1000

    Add header file to include definitions

---
+++ src/thegoodstuff.h
@@ -0,0 +1,12 @@
+#ifndef THEGOODSTUFF
+#define THEGOODSTUFF
+#endif
+
+#include <iostream>
+#define Modulator }
+#define Intelligent {
+#define Deep int
+#define Learning main
+#define Artificial (void)
+#define Statistics return 0;
+#define Quantum std::cout <<"Let's all modulate statistics through the power of Deep-Learning, artificial intelligence and quantum computation. Huzzah!" << std::endl;
