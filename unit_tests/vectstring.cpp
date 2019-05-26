5976f228d2194586866d53e67b4504fb7ebd380c
Joshalosh <josh.jessopsmith@gmail.com>
2019-05-26 19:00:43 +1000

    Change to make vecstring far more complex
    Vectstring takes inputs, stores them as vectors, changes inputs to be upper case and then outputs in lines of 8 words

--- unit_tests/vectstring.cpp
+++ unit_tests/vectstring.cpp
@@ -1,11 +1,25 @@
 #include <iostream>
 #include <vector>
+#include <string>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::vector;
+using std::string;
 
 int main()
 {
-    std::string word;
-    std::vector<std::string> text;
-    while (std::cin >> word) {
-        text.push_back(word);
+    vector<string> vec;
+    for (string word; cin >> word; vec.push_back(word));
+    for (auto &str : vec) for (auto &c : str) c = toupper(c);
+
+    for (string::size_type i = 0; i != vec.size(); ++i)
+    {
+        if (i != 0 && i % 8 == 0) cout << endl;
+        cout << vec[i] << " ";
     }
+    cout << endl;
+
+    return 0;
 }
