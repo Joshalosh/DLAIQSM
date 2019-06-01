ec28408dd43dfe98f000f3cf1bb0c9e7596817a3
Joshalosh <josh.jessopsmith@gmail.com>
2019-06-01 18:31:27 +1000

    Remove duplicate code in vectdef.cpp

--- unit_tests/vectdef.cpp
+++ unit_tests/vectdef.cpp
@@ -1,22 +1,24 @@
 #include <iostream>
 #include <vector>
 
+
+void PrintVect(std::vector<int> v)
+{
+    std::cout << "These are the vector contents" << std::endl;
+    for (auto i : v)
+        std::cout << i << std::endl;
+}
+
 int main()
 {
     std::vector<int> v1(10,42);
     std::vector<int> v2{42,42,42,42,42,42,42,42,42,42};
     std::vector<int> v3 = v2;
-   
-    std::cout << "These are the contents of the first vector" << std::endl; 
-    for (auto i : v1)
-        std::cout << i << std::endl;
-    std::cout << "These are the contents of the second vector" << std::endl;
-    for (auto i : v2)
-        std::cout << i << std::endl;
-    std::cout << "These are the contents of the third vector" << std::endl;
-    for (auto i : v3)
-        std::cout << i << std::endl;
-    
+  
+    PrintVect(v1);
+    PrintVect(v2);
+    PrintVect(v3);
+
     return 0;
 }
 
