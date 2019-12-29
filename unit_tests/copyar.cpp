96022fb2f0fcf64688b4eb934d9fa83f35cf23b0
Joshalosh <josh.jessopsmith@gmail.com>
2019-12-29 13:32:07 +1100

    Modify copyar to work with vectors

--- unit_tests/copyar.cpp
+++ unit_tests/copyar.cpp
@@ -1,15 +1,16 @@
 #include <iostream>
+#include <vector>
 
 int main()
 {
-  int arr1[11];
-  int arr2[10];
+  std::vector<int> arr1;
+  std::vector<int> arr2;
 
-  for (int i = 0; i <= 10; i++)
-    arr1[i] = i;
+  for (int i = 0; i <=10; i++)
+    arr1.push_back(i);
       
   for (auto i : arr1)
-    arr2[i] = i;
+    arr2.push_back(i);
 
   std::cout << "The contents of arr1 are " << std::endl;
   for (auto i : arr1)
