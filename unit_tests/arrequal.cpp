85d6c652517a4ad802711dfa9a6fb15f8813e0b9
Joshalosh <josh.jessopsmith@gmail.com>
2020-01-06 20:59:26 +1100

    Optimise solution and add vector support

--- unit_tests/arrequal.cpp
+++ unit_tests/arrequal.cpp
@@ -1,21 +1,42 @@
 #include <iostream>
+#include <vector>
 
 int main()
 {
-  int count1 = 0;
-  int count2 = 0;
-  int arr1[10] = {0,1,2,3,4,5,6,7,8,9};
-//  int arr2[10] = {0,1,2,3,4,5,6,7,8,9};
-  int arr3[10] = {0,1,2,3,0,4,6,7,8,9};
+  int arr1[5];
+  int arr2[5];
 
-  for (int i = 0; i < 10; i++){
-    count1 += arr1[i];
-    count2 += arr3[i];
+  for (int i = 0; i != 5; ++i)
+    std::cin >> arr1[i];
+  for (int i = 0; i != 5; ++i)
+    std::cin >> arr2[i];
+
+  int i = 0;
+  for (; i != 5; ++i)
+    if (arr1[i] != arr2[i]) {
+      std::cout << "The arrays are different" << std::endl;
+      break;
+    }
+    if (i == 5)
+      std::cout << "The arrays are the same" << std::endl;
+
+
+    std::vector<int> v1;
+    std::vector<int> v2;
+    int t;
+    for (int i = 0; i != 5; ++i) {
+      std::cin >> t;
+      v1.push_back(t);
+    }
+    for (int i = 0; i != 5; ++i) {
+      std::cin >> t;
+      v2.push_back(t);
   }
-  if (count1 != count2) {
-    std::cout << "The arrays are different" << std::endl;
-  } else {
-    std::cout << "The arrays are the same" << std::endl;
-  }
+
+    if (v1 == v2)
+      std::cout << "The vectors are equal" << std::endl;
+    else
+      std::cout << "The vectors are not equal" << std::endl;
+
   return 0;
 }
